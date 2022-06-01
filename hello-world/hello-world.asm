deac82e2c7a16895dd8cfeeb9b23e99bc6b6760f
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-01 19:55:40 +0200

    [add] Hello world in asm

---
+++ hello-world/hello-world.asm
@@ -0,0 +1,22 @@
+;;; hello_world.asm
+
+global _start                   ; Stating that _start is the main function
+
+;;; Instructions section
+section .text:
+_start:
+        mov eax, 4              ; use the write syscall
+        mov ebx, 1              ; use stdout as the fd
+        mov ecx, message        ; use the message as the buffer
+        mov edx, message_length ; and supply the length
+        int 0x80                ; invoke syscall
+
+        ;; Exit
+        mov eax, 1              ; use the exit syscall
+        mov ebx, 0              ; exit succesfully
+        int 0x80                ; invoke syscall
+
+;;; Variable section
+section .data:
+        message db "Hello World!", 0xA ; String ending in \n
+        message_length equ $-message    ; Length of the string above
