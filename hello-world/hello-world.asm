86242860e5079bd77b9b525184b46ad84aea61ef
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-02 15:17:17 +0200

    [remove] Unnecessary ':' in section

--- hello-world/hello-world.asm
+++ hello-world/hello-world.asm
@@ -3,7 +3,7 @@
 global _start                   ; Stating that _start is the main function
 
 ;;; Instructions section
-section .text:
+section .text
 _start:
         mov eax, 4              ; use the write syscall
         mov ebx, 1              ; use stdout as the fd
@@ -17,6 +17,6 @@ _start:
         int 0x80                ; invoke syscall
 
 ;;; Variable section
-section .data:
+section .data
         message db "Hello World!", 0xA ; String ending in \n
         message_length equ $-message    ; Length of the string above
