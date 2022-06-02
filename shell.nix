f3b359cee3cf806063adb57a254f9bc3a3c0ddc6
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-02 11:58:00 +0200

    [add] Simple bash function for easier assembling and linking

--- shell.nix
+++ shell.nix
@@ -7,4 +7,14 @@ pkgs.mkShell {
     nasm
     gcc
   ];
+
+  shellHook = ''
+  comp-asm () {
+    file=$1
+    file="''${file%.*}"
+    nasm -f elf32 "$file.asm" \
+      && ld -m elf_i386 "$file.o" -o "$file" \
+      && echo "$file assembled and linked succesfully!"
+  }
+  '';
 }
