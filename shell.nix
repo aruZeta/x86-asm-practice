aaecdfd9da168e00dbc55a84b9342fb5256d1602
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-03 22:50:32 +0200

    [add] Debugging assembly with gdb

--- shell.nix
+++ shell.nix
@@ -1,18 +1,18 @@
 { pkgs ? import <nixpkgs> {}
 , ...
 }:
-
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nasm
     gcc
+    gdb
   ];
 
   shellHook = ''
   comp-asm () {
     file=$1
     file="''${file%.*}"
-    nasm -f elf32 "$file.asm" \
+    nasm -f elf32 -g -F dwarf "$file.asm" \
       && ld -m elf_i386 "$file.o" -o "$file" \
       && echo "$file assembled and linked succesfully!"
   }
